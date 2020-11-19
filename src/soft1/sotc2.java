fc1eb3da0733c9cdabea9e83073cc3973f302d10
NOTEPC <NOTEPC@Sys>
2020-11-19 10:04:05 +0530

    softc2 commit

---
+++ src/soft1/sotc2.java
@@ -0,0 +1,11 @@
+package soft1;
+
+public class sotc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aa");
+		System.out.println("cccccc");
+	}
+
+}
