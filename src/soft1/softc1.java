6b3856f1333fdb01f355a49cf31853be8dafec08
NOTEPC <NOTEPC@Sys>
2020-11-19 09:59:48 +0530

    soft1 commited

---
+++ src/soft1/softc1.java
@@ -0,0 +1,11 @@
+package soft1;
+
+public class softc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aa");
+		System.out.println("bb");
+	}
+
+}
